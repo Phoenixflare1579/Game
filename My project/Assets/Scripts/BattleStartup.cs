a7d8ddcae1de634b343146d27ccc2fb444b1fd9b
Phoenixflare1579 <114099630+Phoenixflare1579@users.noreply.github.com>
2023-10-12 12:51:23 -0400

    Update to organize turn order

--- My project/Assets/Scripts/BattleStartup.cs
+++ My project/Assets/Scripts/BattleStartup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,8 @@ public class BattleStartup : MonoBehaviour
     public string[] Turn;
     public int order = 0;
     public int[] randomizer;
+    int temp;
+    string t;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,18 +33,29 @@ public class BattleStartup : MonoBehaviour
         {
             Turn[i] = (GameObject.FindGameObjectsWithTag("Player")[i].name);
             if (GameObject.FindGameObjectsWithTag("Player")[i].GetComponent<KrysStats>() != null)
-                randomizer[i] = GameObject.FindGameObjectsWithTag("Player")[i].GetComponent<KrysStats>().Speed + Random.Range(0, 100);
+                randomizer[i] = GameObject.FindGameObjectsWithTag("Player")[i].GetComponent<KrysStats>().Speed + UnityEngine.Random.Range(0, 100);
             else if (GameObject.FindGameObjectsWithTag("Player")[i].GetComponent<MCStats>() != null)
-                randomizer[i] = GameObject.FindGameObjectsWithTag("Player")[i].GetComponent<MCStats>().Speed + Random.Range(0, 100);
+                randomizer[i] = GameObject.FindGameObjectsWithTag("Player")[i].GetComponent<MCStats>().Speed + UnityEngine.Random.Range(0, 100);
         }
         for (int i = GameObject.FindGameObjectsWithTag("Player").Length; i < GameObject.FindGameObjectsWithTag("Enemy").Length + GameObject.FindGameObjectsWithTag("Player").Length - 1; i++)
         {
             Turn[i] = (GameObject.FindGameObjectsWithTag("Enemy")[i].name);
-            randomizer[i] = GameObject.FindGameObjectsWithTag("Enemy")[i].GetComponent<EnemyStats>().Speed + Random.Range(0, 100);
+            randomizer[i] = GameObject.FindGameObjectsWithTag("Enemy")[i].GetComponent<EnemyStats>().Speed + UnityEngine.Random.Range(0, 100);
         }
         for (int i = 0; i < GameObject.FindGameObjectsWithTag("Enemy").Length + GameObject.FindGameObjectsWithTag("Player").Length - 1; i++)
         {
-            //I got the speed saved but we have to organize the array in greatest to least and then order the turn array in that same order.
+            for (int j = i; j < GameObject.FindGameObjectsWithTag("Enemy").Length + GameObject.FindGameObjectsWithTag("Player").Length - 1; j++)
+            {
+                if (randomizer[i] < randomizer[j])
+                {
+                    temp=randomizer[i];
+                    randomizer[i] = randomizer[j];
+                    randomizer[j] = temp;
+                    t = Turn[i];
+                    Turn[i] = Turn[j];
+                    Turn[j] = t;
+                }
+            }
         }
     }
     private void Update()
