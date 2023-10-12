482af10bd50d398238136780a6f7c6dcfc711f56
Victor-Valero <vvalero9218@floridapoly.edu>
2023-10-11 23:11:24 -0400

    Turn Logic in place, not implemented yet

--- My project/Assets/Scripts/BattleStartup.cs
+++ My project/Assets/Scripts/BattleStartup.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BattleStartup : MonoBehaviour
@@ -8,10 +10,12 @@ public class BattleStartup : MonoBehaviour
     public GameObject[] playerP;
     public GameObject[] enemyP;
     public string[] Turn;
+    public string[][] inOrder;
     public int order = 0;
     // Start is called before the first frame update
     void Start()
     {
+
         if (GameObject.FindGameObjectsWithTag("Player").Length == 1)
         {
             for (int i = 0; i < playerP.Length; i++)
@@ -24,16 +28,37 @@ public class BattleStartup : MonoBehaviour
         {
             Instantiate(enemyP[i]);
         }
-        Turn = new string[GameObject.FindGameObjectsWithTag("Player").Length+ GameObject.FindGameObjectsWithTag("Enemy").Length];
+
+        Turn = new string[GameObject.FindGameObjectsWithTag("Player").Length-1+ GameObject.FindGameObjectsWithTag("Enemy").Length];
+
+        inOrder = new string[GameObject.FindGameObjectsWithTag("Player").Length - 1 + GameObject.FindGameObjectsWithTag("Enemy").Length][];
+
         for (int i = 0; i < GameObject.FindGameObjectsWithTag("Player").Length; i++)
-            Turn[i]=(GameObject.FindGameObjectsWithTag("Player")[i].name);
-        for(int i = GameObject.FindGameObjectsWithTag("Player").Length; i < GameObject.FindGameObjectsWithTag("Enemy").Length+GameObject.FindGameObjectsWithTag("Player").Length-1; i++)
-            Turn[i]=(GameObject.FindGameObjectsWithTag("Enemy")[i].name);
-        // for (int i = 0; i<Turn.Length; i++)//Victor do you know how to randomize the turn order array and have a bias based on speed?
         {
-            //int Randomize=Random.Range(0,Turn.Length);
-            //Turn[i]=
+            Turn[i] = (GameObject.FindGameObjectsWithTag("Player")[i].name);
         }
+            
+        for(int i = GameObject.FindGameObjectsWithTag("Player").Length; i < GameObject.FindGameObjectsWithTag("Enemy").Length+GameObject.FindGameObjectsWithTag("Player").Length-1; i++)
+        {
+            Turn[i] = (GameObject.FindGameObjectsWithTag("Enemy")[i].name);
+        }
+
+        for (int i = 0; i< Turn.Length; i++)//Speed Random Bais
+        {
+            int CharSpeed = (GameObject.Find(Turn[i]).GetComponent<CharStats>().Speed + Random.Range(0, 100));
+
+            inOrder[i] = new string[] { Turn[i], CharSpeed.ToString() };
+
+        }
+
+        var sorted = inOrder.OrderByDescending(y => y[1]);
+
+        foreach (string[] inOrder in sorted) 
+        {
+            Debug.Log(inOrder[0]);
+            Debug.Log(inOrder[1]);
+        }
+
     }
     private void Update()
     {
