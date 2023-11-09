dc436cb268c3339e5f75de4a27323be2ffc22bbb
Phoenixflare1579 <114099630+Phoenixflare1579@users.noreply.github.com>
2023-11-09 15:13:17 -0500

    Save Stations Created and Interaction Created

--- My project/Assets/Scripts/SaveManager.cs
+++ My project/Assets/Scripts/SaveManager.cs
@@ -1,13 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Windows;
 
-public class SaveManager : MonoBehaviour
+public class SaveManager : MonoBehaviour,IInteractable
 {
+    CharStats MC;
+    CharStats Krys;
+    private void Start()
+    {
+        MC = GameObject.Find("MC").GetComponent<CharStats>();
+        Krys = GameObject.Find("Krys").GetComponent<CharStats>();
+    }
+
     public void Save()
     {
-        SaveS.SaveData(GameObject.Find("MC").GetComponent<CharStats>(), GameObject.Find("Krys").GetComponent<CharStats>());
+        SaveS.SaveData(MC,Krys);
+    }
+
+    public void Load()
+    {
+        Data data = SaveS.LoadData();
+        MC.Level = data.levelM;
+        MC.EXP = data.expM;
+        MC.skillpoints = data.skillpointsM;
+        MC.HP = data.healthM;
+        MC.Mana = data.manaM;
+        MC.CharName = data.nameM;
+        MC.transform.position = new Vector3(data.position[0], data.position[1], data.position[2]);
+        Krys.Level = data.levelK;
+        Krys.EXP = data.expK;
+        Krys.skillpoints = data.skillpointsK;
+        Krys.HP = data.healthK;
+        Krys.Mana = data.manaK;
+    }
+
+    public void Interact()
+    {
+        Save();
     }
 }
