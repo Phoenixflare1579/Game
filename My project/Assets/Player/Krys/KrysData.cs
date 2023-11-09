05800a3021a3c2144934fd5959323e91f18e7516
Phoenixflare1579 <114099630+Phoenixflare1579@users.noreply.github.com>
2023-11-09 11:53:27 -0500

    U

--- My project/Assets/Player/Krys/KrysData.cs
+++ My project/Assets/Player/Krys/KrysData.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
+[System.Serializable]
 public class KrysData
 {
     public int level;
@@ -10,4 +12,12 @@ public class KrysData
     public int skillpoints;
     public int health;
     public int mana;
+    public KrysData(KrysStats stats)
+    {
+        level = stats.Level;
+        exp = stats.EXP;
+        skillpoints = stats.skillpoints;
+        health = stats.HP;
+        mana = stats.Mana;
+    }
 }
