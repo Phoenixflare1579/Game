445c94df0133ab09d872c42786cf5b391afb253d
Phoenixflare1579 <114099630+Phoenixflare1579@users.noreply.github.com>
2023-11-12 08:56:58 -0500

    Added Spears

---
+++ My project/Assets/Scripts/Items/Equipment/Weapons/Spear/Pike.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pike : Equipment
+{
+    void Start()
+    {
+        HP = 0;
+        Speed = 0;
+        Def = 0;
+        PhysAtk = 15;
+        MagicAtk = 0;
+        MagicDef = 0;
+        Mana = 0;
+        Evasion = 0;
+        Accuracy = 0;
+        Crit = 0;
+        CritDmg = 0;
+        Name = "Pike";
+        Type = "Spear";
+    }
+}
